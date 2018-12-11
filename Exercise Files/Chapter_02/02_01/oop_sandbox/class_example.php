32a7d4c9bf1359eba0b7c9cf018823a96b1087ea
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-12-11 16:21:12 +0200

    Learned how to define a class

---
+++ Exercise Files/Chapter_02/02_01/oop_sandbox/class_example.php
@@ -0,0 +1,18 @@
+<?php
+
+class Student {
+
+}
+
+$classes = get_declared_classes();
+echo "Clasess: " . implode(',', $classes). "<br />";
+
+$class_names = ['Product', 'Student', 'student'];
+foreach ($class_names as $class_name) {
+    if(class_exists($class_name)) {
+        echo "{$class_name} is a declared class.<br />";
+    } else {
+        echo "{$class_name} is not a declared class.<br />";
+    }
+}
+?>
