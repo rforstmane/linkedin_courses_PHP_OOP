c5877b1310a1104cf57994f922e7fa65b63f7712
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-12-15 16:32:37 +0200

    Learned about  class inheritance

--- Exercise Files/Chapter_03/03_03/oop_sandbox/class_inheritance.php
+++ Exercise Files/Chapter_03/03_03/oop_sandbox/class_inheritance.php
@@ -2,6 +2,8 @@
 
 class User {
 
+  var $is_admin = false;
+
   var $first_name;
   var $last_name;
   var $username;
@@ -9,10 +11,25 @@ class User {
   function full_name() {
     return $this->first_name . " " . $this->last_name;
   }
-
 }
 
 class Customer extends User {
+
+  var $city;
+  var $state;
+  var $country;
+
+  function location() {
+    return $this->city . ", " . $this->state . ", " . $this->country;
+  }
+}
+
+class AdminUser extends User {
+  var $is_admin = true;
+
+    function full_name() {
+        return $this->first_name . " " . $this->last_name . " (Admin)";
+    }
 }
 
 $u = new User;
@@ -24,9 +41,15 @@ $c = new Customer;
 $c->first_name = 'George';
 $c->last_name = 'Costanza';
 $c->username = 'gcostanza';
+$c->city = 'New York';
+$c->state= 'New York';
+$c->country = 'United Sates';
 
 echo $u->full_name() . '<br />';
 echo $c->full_name() . '<br />';
+
+echo $c->location() . '<br />';
+//echo $u->location() . '<br />'; no method error
 
 echo get_parent_class($u) . '<br />';
 echo get_parent_class($c) . '<br />';
