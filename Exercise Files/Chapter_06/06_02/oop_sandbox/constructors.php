5c95e8d74e575a47b934896d404a98841da79840
rforstmane <35078285+rforstmane@users.noreply.github.com>
2018-12-15 16:34:32 +0200

    Learned about constructor arguments

--- Exercise Files/Chapter_06/06_02/oop_sandbox/constructors.php
+++ Exercise Files/Chapter_06/06_02/oop_sandbox/constructors.php
@@ -7,8 +7,10 @@ class Sofa {
   public $seats = 3;
   public $arms = 2;
 
-  public function __construct() {
+  public function __construct($args=[]) {
     self::$instance_count++;
+    $this->seats = isset($args['seats']) ? $args['seats'] : $this->seats;
+    $this->arms = isset($args['arms']) ? $args['arms'] : $this->arms;
   }
 
 }
@@ -21,19 +23,19 @@ class Loveseat extends Sofa {
   var $seats = 2;
 }
 
-$sofa = new Sofa();
+$sofa = new Sofa(['seats' => 3, 'arms' => 2]);
 echo 'Sofa<br />';
 echo '- seats: ' . $sofa->seats . '<br />';
 echo '- arms: ' . $sofa->arms . '<br />';
 echo '<br />';
 
-$couch = new Couch();
+$couch = new Couch(['seats' => 4]);
 echo 'Couch<br />';
 echo '- seats: ' . $couch->seats . '<br />';
 echo '- arms: ' . $couch->arms . '<br />';
 echo '<br />';
 
-$loveseat = new Loveseat();
+$loveseat = new Loveseat(['arms' => 0]);
 echo 'Loveseat<br />';
 echo '- seats: ' . $loveseat->seats . '<br />';
 echo '- arms: ' . $loveseat->arms . '<br />';
